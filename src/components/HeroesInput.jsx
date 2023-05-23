d73d920a948e7576ebc00e176d301a334326e181
edson-mac <edson-mac@hotmail.com>
2023-05-23 17:13:58 -0300

    audios fixados, css base, logicas de render

--- src/components/HeroesInput.jsx
+++ src/components/HeroesInput.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import PropTypes from "prop-types";
 import { useProvider } from "../context/Provider";
 
-export default function HeroesInput({ heroes, type, guessed }) {
+export default function HeroesInput({ heroes, type }) {
   const [value, setValue] = useState("");
   const [renderList, setRenderList] = useState(false);
   const { submitQuoteHero } = useProvider();
@@ -26,10 +26,10 @@ export default function HeroesInput({ heroes, type, guessed }) {
   };
 
   return (
-    <div className="App">
+    <div className="flex flex-col pt-4 gap-2 relative">
+    <div className="flex flex-row pt-4 gap-2">
       <div className="search-container">
         <input
-          disabled={guessed}
           type="text"
           id="default-input"
           value={value}
@@ -37,43 +37,48 @@ export default function HeroesInput({ heroes, type, guessed }) {
           className="w-60 bg-gray-700 border border-gray-400 text-gray-200 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
         />
       </div>
-      {renderList && (
-        <div className="dropdown w-64 max-h-56 scrollbar-thin scrollbar-thumb-rose-800 scrollbar-track-rose-950 overflow-auto">
-          {heroes
-            .filter((val) => {
-              if (value == "") {
-                return null;
-              } else if (
-                val.name.toLowerCase().startsWith(value.toLowerCase())
-              ) {
-                return val;
-              }
-            })
-            .map((val) => {
-              return (
-                <div
-                  onClick={onClick1}
-                  className="dropdown-row flex row gap-2 p-2 bg-gray-700 transition ease-in-out delay-[10ms] hover:bg-gray-500"
-                  key={val.name}
-                >
-                  <img
-                    className="w-14 border-solid border-2 border-black"
-                    src={val.img}
-                    alt={val.name}
-                  />
-                  <p className="text-neutral-50 pt-1">{val.name}</p>
-                </div>
-              );
-            })}
-        </div>
-      )}
-      <button
-        disabled={!heroes.find((e) => e.name === value)}
-        onClick={() => submitChampion(value)}
-      >
-        Enviar
-      </button>
+
+      <button disabled={!heroes.find((e) => e.name === value)}
+        onClick={() => submitChampion(value)} type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm p-2.5 text-center inline-flex items-center mr-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+  <svg aria-hidden="true" className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
+  <span className="sr-only">Icon description</span>
+</button>
     </div>
+
+{renderList && (
+  <div className="absolute pt-16 mr-10 flex items-center justify-center z-50"> 
+  <div className="dropdown w-64 max-h-56 scrollbar-thin scrollbar-thumb-rose-800 scrollbar-track-rose-950 overflow-auto">
+    {heroes
+      .filter((val) => {
+        if (value == "") {
+          return null;
+        } else if (
+          val.name.toLowerCase().startsWith(value.toLowerCase())
+        ) {
+          return val;
+        }
+      })
+      .map((val) => {
+        return (
+          <div
+            onClick={onClick1}
+            className="dropdown-row flex row gap-2 p-2 bg-gray-700 transition ease-in-out delay-[10ms] hover:bg-gray-500"
+            key={val.name}
+          >
+            <img
+              className="w-14 border-solid border-2 border-black"
+              src={val.img}
+              alt={val.name}
+            />
+            <p className="text-neutral-50 pt-1">{val.name}</p>
+          </div>
+        );
+      })}
+  </div>
+  </div>
+)}
+
+</div>
   );
 }
 
--- src/pages/QuoteGame.jsx
+++ src/pages/QuoteGame.jsx
@@ -1,69 +1,136 @@
 import HeroesInput from "../components/HeroesInput";
 import { useEffect, useState } from "react";
 import { useProvider } from "../context/Provider";
-import ReactLoading from 'react-loading';
-import AudioPlayer from "../components/AudioPlayer";
+import ReactLoading from "react-loading";
 
 function QuoteGame() {
   const { heroes, quoteTries, quoteStatus } = useProvider();
   const [quoteHeroes, setQuoteHeroes] = useState();
+  const [audioClue, setAudioClue] = useState(0);
+  const [renderInput, setRenderInput] = useState(true);
 
   useEffect(() => {
-    if (heroes) {
-      setQuoteHeroes(heroes)
-    }
-  }, [heroes]);
-  
-  useEffect(() => {
     if (quoteTries && heroes) {
-      const filteredHeroes = heroes.filter((e) => !quoteTries.includes(e.name))
-      setQuoteHeroes(filteredHeroes)
+      const filteredHeroes = heroes.filter((e) => !quoteTries.includes(e.name));
+      setQuoteHeroes(filteredHeroes);
     }
   }, [quoteTries, heroes]);
 
-
+  useEffect(() => {
+    if (quoteTries && quoteTries.includes(quoteStatus.todayhero)) {
+      setRenderInput(false);
+    }
+    if (quoteTries) { 
+      setAudioClue(quoteTries.length); }
+  }, [quoteTries, quoteStatus]);
 
   if (heroes && quoteHeroes && quoteTries && quoteStatus) {
-    console.log(quoteStatus.audiolink)
+    console.log(quoteStatus.audiolink);
+
+    const playSound = () => {
+      var audio = new Audio(
+        quoteStatus.audiolink.substring(
+          0,
+          quoteStatus.audiolink.indexOf(".mp3") + 4
+        )
+      );
+      audio.play();
+    };
+
     return (
-      <div className="text-white">
-        <div>QuoteGame</div>
-        <div>
-          <p className="text-xl text-white">{quoteStatus.quote}</p>
+      <div className="text-white flex flex-col lg:mt-56 items-center content-center">
+        <div className="border border-r-2 border-red-500 flex flex-col items-center content-center">
+          <h1 className="text-2xl">Which hero says</h1>
+
+          <p className="text-4xl w-8/12 text-white pt-4 pb-4">
+            ❝{quoteStatus.quote}❞
+          </p>
+
+          <div>
+            <>
+            { renderInput && audioClue >= 5 && <div>
+               <img
+                className="h-12"
+                src="https://cdn-icons-png.flaticon.com/512/2468/2468825.png"
+                onClick={() => playSound()}
+              />
+              <p>Audio Clue</p>
+            </div> }
+            { renderInput && audioClue < 5 && 
+            <div>
+              <img
+                className="h-12 grayscale"
+                src="https://cdn-icons-png.flaticon.com/512/2468/2468825.png"
+              />
+              <p>Audio Clue in {5 - audioClue } tries</p>
+            </div>
+            }
+            </>
+          </div> 
+
+
         </div>
+        {renderInput && (
+          <HeroesInput
+            heroes={quoteHeroes}
+            type={"quote"}
+            guessed={quoteTries.includes(quoteStatus.todayhero)}
+          />
+        )}
         <div>
-          <AudioPlayer src={quoteStatus.audiolink}/>
+          {quoteStatus ? (
+            <p>{`${quoteStatus.count} people already found out`}</p>
+          ) : (
+            <p>carregando</p>
+          )}
         </div>
-        <HeroesInput heroes={quoteHeroes} type={"quote"}  guessed={quoteTries.includes(quoteStatus.todayhero)}/>
+
         <div>
-          {quoteStatus? <p>{`${quoteStatus.count} Pessoas já acertaram`}</p> : <p>carregando</p>}
+          {quoteTries.map((heroName) => {
+            const hero = heroes.find((e) => e.name === heroName);
+
+            if (!hero) return null;
+            if (heroName === quoteStatus.todayhero)
+              return (
+                <div
+                  className="flex flex-col items-center border-4 border-zinc-50 pt-2 w-64 h-20 mt-2 bg-green-600"
+                  key={hero}
+                >
+                  <img
+                    className="w-16 border-solid border-2 border-black"
+                    src={hero.img}
+                  />
+                  <span>{heroName}</span>
+                </div>
+              );
+            else
+              return (
+                <div
+                  className="flex flex-col items-center pt-4 border border-zinc-300 w-64 h-20 mt-2 bg-red-600"
+                  key={hero}
+                >
+                  <img
+                    className="w-16 border-solid border-2 border-black"
+                    src={hero.img}
+                  />
+                  <span>{heroName}</span>
+                </div>
+              );
+          })}
         </div>
-        <div>
-          {
-            quoteTries
-              .map((heroName) => {
-                const hero = heroes.find((e) => e.name === heroName)
-                return (
-                  <div key={hero}>
-                    <img src={hero.img} width={"100px"}/>
-                    <span>{heroName}</span>
-                  </div>
-                )
-              })
-          }
-        </div>
-        <div>
-          {quoteTries.includes(quoteStatus.todayhero) ? <p>Acertou!</p> : <></>}
-        </div>
+        <div>{!renderInput && <p>Acertou!</p>}</div>
       </div>
     );
   }
 
   return (
-    <ReactLoading type={"spinningBubbles"} color={"white"} height={100} width={100} />
-  )
-
-  
+    <ReactLoading
+      type={"spinningBubbles"}
+      color={"white"}
+      height={100}
+      width={100}
+    />
+  );
 }
 
 export default QuoteGame;
