ab20f9d305b72afd9148d41b71245845a5d3dfb3
guilhermeSCampos <guilhermeregular@gmail.com>
2023-05-24 18:39:21 -0300

    classic game input

--- src/pages/ClassicGame.jsx
+++ src/pages/ClassicGame.jsx
@@ -1,9 +1,13 @@
 import HeroesInput from "../components/HeroesInput"
+import { useProvider } from "../context/Provider"
 
 function ClassicGame() {
+
+  const { heroes } = useProvider();
+
   return (
-    <div>ClassicGame
-    <HeroesInput />
+    <div className="text-white">ClassicGame
+    <HeroesInput heroes={heroes} type="classic"/>
     </div>
   )
 }
